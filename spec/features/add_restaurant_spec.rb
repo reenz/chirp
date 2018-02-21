e9b096337ac6f16393b34dd8004878f07ea5276c
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-21 17:00:19 +0000

    Passes validation of name when creating a restaurant
    co-authored-by: Tagrand <24781073+Tagrand@users.noreply.github.com>

--- spec/features/add_restaurant_spec.rb
+++ spec/features/add_restaurant_spec.rb
@@ -14,9 +14,9 @@ feature 'Add new restaurant' do
     click_link 'New Restaurant'
     fill_in :Description, :with => 'Yuuuuuuge portions!'
     click_button 'Add Restaurant'
-    expect(page).to have_current_path('/restaurants/new')
+    expect(page).to have_current_path('/restaurants/new?notice=Must+enter+a+restaurant+name.')
     expect(page).to have_content('Must enter a restaurant name.')
-    expect(find_field('description').value).to eq 'Yuuuuuuge portions!'
+    expect(find_field('Description').value).to eq 'Yuuuuuuge portions!'
     expect(Restaurant.find_by description: 'Yuuuuuuge portions!').to be_nil
   end
 end
