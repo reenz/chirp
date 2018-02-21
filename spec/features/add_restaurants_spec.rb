f33b23cc2e5909c413c4edef193053cb70170a43
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-02-21 11:06:41 +0000

    Add destroy restaurant test
    co-authored-by: Tagrand <24781073+Tagrand@users.noreply.github.com>
    co-authored-by: Reena <reena.sanjay.sharma@gmail.com>
    co-authored-by: domvernon <3686359+domvernon@users.noreply.github.com>

--- spec/features/add_restaurants_spec.rb
+++ spec/features/add_restaurants_spec.rb
@@ -2,16 +2,8 @@ require 'rails_helper'
 
 feature 'Add new restaurant' do
   scenario 'add multiple restaurants' do
-    visit 'restaurants'
-    click_link 'New Restaurant'
-    fill_in :Name, :with => 'Rocket Sausages'
-    fill_in :Description, :with => 'The best vegan sausages ever!! (Not veggie).'
-    click_button 'Add Restaurant'
-    click_link 'New Restaurant'
-    fill_in :Name, :with => 'Super Meat Store'
-    fill_in :Description, :with => 'Ironically this is a big vegan restaurant too.
-    But a lot of confused meat eaters visit and a are highly disappointed...'
-    click_button 'Add Restaurant'
+    add_restaurant('Rocket Sausages', 'The best vegan sausages ever!! (Not veggie).')
+    add_restaurant('Super Meat Store', 'Ironically this is a big vegan restaurant too. But a lot of confused meat eaters visit and a are highly disappointed...')
     expect(page).to have_content('Rocket Sausages')
     expect(page).to have_content('The best vegan sausages ever!! (Not veggie).')
     expect(page).to have_content('Super Meat Store')
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,3 +60,22 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+RSpec.configure do |config|
+ # Everything in this block runs once before all the tests run
+ config.before(:suite) do
+   DatabaseCleaner.strategy = :transaction
+   DatabaseCleaner.clean_with(:truncation)
+ end
+
+ # Everything in this block runs once before each individual test
+ config.before(:each) do
+   DatabaseCleaner.start
+ end
+
+ # Everything in this block runs once after each individual test
+ config.after(:each) do
+   DatabaseCleaner.clean
+ end
+
+end
