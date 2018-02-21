55da27bb91202673e79e7603242d0a990a247f07
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-21 15:25:43 +0000

    unit test for database name validation
    co-authored-by: Tagrand <24781073+Tagrand@users.noreply.github.com>

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Restaurant, type: :model do
   describe 'Validations' do
     it "Can't add a restaurant without a name" do
-      
+      expect{ Restaurant.create(description: 'a description') }.to_not change{Restaurant.count}
     end
   end
 end
