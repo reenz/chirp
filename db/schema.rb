1d014dcd86826d25b2e2c92cb020f5148a782a07
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-02-21 14:51:03 +0000

    Add new model Review
    co-authored-by: Tagrand <24781073+Tagrand@users.noreply.github.com>
    co-authored-by: Reena <17711442+reenz@users.noreply.github.com>
    co-authored-by: domvernon <3686359+domvernon@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220124021) do
+ActiveRecord::Schema.define(version: 20180221144642) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
