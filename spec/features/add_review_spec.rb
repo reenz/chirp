0e8ecb6c4941b529f7fd697349c1b394307ca641
Tagrand <thomas.a.grand@gmail.com>
2018-02-22 11:12:28 +0000

    test reviews are stored in database

--- spec/features/add_review_spec.rb
+++ spec/features/add_review_spec.rb
@@ -1,4 +1,11 @@
 feature 'Add a review' do
+  scenario 'Store review in database' do
+    add_restaurant('Meat and Egg', 'Poached eggs on a lovely steak')
+    page.all(:link, 'Show')[0].click
+    fill_in "review[rating]", :with => 5
+    expect{ click_button('Create Review') }.to change{Review.count}
+  end
+
   scenario 'Add a review for an existing restaurant' do
     add_restaurant('Meat and Egg', 'Poached eggs on a lovely steak')
     add_restaurant('Lovely Veggies', 'Home-grown stuff for veggie lovers')
