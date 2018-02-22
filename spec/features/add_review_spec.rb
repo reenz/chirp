779acbbe2fdc71f51f55008bf5cccfdb55404967
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-22 14:34:29 +0000

    adds failing test for rating validation
    co-authored-by: Reena <17711442+reenz@users.noreply.github.com>

--- spec/features/add_review_spec.rb
+++ spec/features/add_review_spec.rb
@@ -2,7 +2,7 @@ feature 'Add a review' do
   scenario 'Store review in database' do
    add_restaurant('Meat and Egg', 'Poached eggs on a lovely steak')
    page.all(:link, 'Show')[0].click
-   expect{ add_rating(5) }.to change{Review.count}
+   expect{ add_rating(5) }.to change{ Review.count }
  end
 
   scenario 'Add a review for an existing restaurant' do
@@ -16,4 +16,20 @@ feature 'Add a review' do
     expect(page).to have_content('Bad vegetables')
     expect(page).not_to have_content('Meat and Egg')
   end
+
+feature 'no rating is added to the review' do
+  before do
+    add_restaurant('Lovely Veggies', 'Home-grown stuff for veggie lovers')
+    click_link 'Show'
+  end
+
+  scenario 'the page displays an error message' do
+    add_comment('I liked this one')
+    expect(page).to have_content('Must provide a rating')
+  end
+
+  scenario 'the review should not be saved' do
+    expect{ add_comment('nice restaurant') }.to_not change{ Review.count }
+  end
+end
 end
