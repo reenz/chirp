edf031b10c67d63bc1eebc005303bc7b3e3fd3d3
Tagrand <thomas.a.grand@gmail.com>
2018-02-22 12:19:05 +0000

    add average rating to page

--- spec/features/add_average_rating_spec.rb
+++ spec/features/add_average_rating_spec.rb
@@ -2,6 +2,9 @@ feature 'show average ratings' do
   scenario 'after adding several ratings, the average should appear on a restaurants page' do
     add_restaurant('Nandos', 'Some chicken in some form')
     page.all(:link, 'Show')[0].click
-    fill_in "review[rating]", :with => 2
+    add_rating(2)
+    add_rating(2)
+    add_rating(5)
+    expect(page).to have_content('3')
   end
 end
