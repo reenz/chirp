28d4ffad68c4d18f77bfc25f3bac74f159f43a8b
Tagrand <thomas.a.grand@gmail.com>
2018-02-22 12:22:28 +0000

    test to see average rating is 1 d.p.

--- spec/features/add_average_rating_spec.rb
+++ spec/features/add_average_rating_spec.rb
@@ -2,9 +2,10 @@ feature 'show average ratings' do
   scenario 'after adding several ratings, the average should appear on a restaurants page' do
     add_restaurant('Nandos', 'Some chicken in some form')
     page.all(:link, 'Show')[0].click
-    add_rating(2)
-    add_rating(2)
     add_rating(5)
-    expect(page).to have_content('3')
+    add_rating(4)
+    add_rating(4)
+    expect(page).to have_content('4.3')
+    expect(page).not_to have_content('4.33')
   end
 end
