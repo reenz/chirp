65041d4e5d389269fde1a9c7e4a264d29193ed12
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-02-22 11:06:10 +0000

    Add successful test for adding a review

--- spec/features/add_review_spec.rb
+++ spec/features/add_review_spec.rb
@@ -3,11 +3,10 @@ feature 'Add a review' do
     add_restaurant('Meat and Egg', 'Poached eggs on a lovely steak')
     add_restaurant('Lovely Veggies', 'Home-grown stuff for veggie lovers')
     page.all(:link, 'Show')[1].click
-    expect(page).to have_content('aaaaaaaaaaaaaaa')
-    fill_in :Rating, :with => 5
+    fill_in "review[rating]", :with => 5
     click_button('Create Review')
-    fill_in :rating, :with => 2
-    fill_in :comment, :with => 'Bad vegetables'
+    fill_in "review[rating]", :with => 2
+    fill_in "review[comment]", :with => 'Bad vegetables'
     click_button('Create Review')
     expect(page).to have_content('5')
     expect(page).to have_content('2')
