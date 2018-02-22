6fb34e3b6e1c6b8f1e138932f32e1e7841d78193
Tagrand <thomas.a.grand@gmail.com>
2018-02-22 12:02:22 +0000

    remove extra requires

--- spec/features/add_average_rating_spec.rb
+++ spec/features/add_average_rating_spec.rb
@@ -2,6 +2,7 @@ feature 'show average ratings' do
   scenario 'after adding several ratings, the average should appear on a restaurants page' do
     add_restaurant('Nandos', 'Some chicken in some form')
     page.all(:link, 'Show')[0].click
-    
+    fill_in "review[rating]", :with => 2
+
   end
 end
