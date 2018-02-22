f52f58b7aaff77f0167840c6418ae6c73b9fe68f
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-02-22 14:56:22 +0000

    Refactor tests for rating

--- spec/features/add_average_rating_spec.rb
+++ spec/features/add_average_rating_spec.rb
@@ -1,20 +1,18 @@
 feature 'show average ratings' do
-  scenario 'after adding several ratings, the average should appear on the individual page' do
+  before(:each) do
     add_restaurant('Nandos', 'Some chicken in some form')
     page.all(:link, 'Show')[0].click
     add_rating(5)
     add_rating(4)
     add_rating(4)
+  end
+
+  scenario 'after adding several ratings, the average should appear on the individual page' do
     expect(page).to have_content('4.3')
     expect(page).not_to have_content('4.33')
   end
 
   scenario 'show average rating for each restaurant on restaurants page' do
-     add_restaurant('Nandos', 'Some chicken in some form')
-     page.all(:link, 'Show')[0].click
-     add_rating(5)
-     add_rating(4)
-     add_rating(4)
      click_link('Back')
      add_restaurant('Japanese Kento', 'Lovely sushi')
      page.all(:link, 'Show')[1].click
@@ -26,9 +24,19 @@ feature 'show average ratings' do
      expect(page).to have_content('3.3')
   end
 
-  scenario 'displays "no rating yet" if there is no rating on the individual page' do
+  scenario 'displays "no rating yet" on the restaurants page' do
+    click_link('Back')
     add_restaurant('Leon','Lots of healthy food')
-    click_link ('Show')
+    expect(page).to have_content('no ratings yet')
+    expect(page).to have_content('Leon')
+    expect(page).not_to have_content('0.0')
+  end
+
+
+  scenario 'displays "no rating yet" if there is no rating on the individual page' do
+    click_link('Back')
+    add_restaurant('Leon','Lots of healthy food')
+    page.all(:link, 'Show')[1].click
     expect(page).to have_content('no ratings yet')
     expect(page).to have_content('Leon')
     expect(page).to have_content('Lots of healthy food')
