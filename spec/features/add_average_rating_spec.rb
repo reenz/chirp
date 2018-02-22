bb700adefdcb6f5f55409004bd790d8e02138702
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-02-22 12:32:30 +0000

    Add partial test for displaying average rating on the restaurant page

--- spec/features/add_average_rating_spec.rb
+++ spec/features/add_average_rating_spec.rb
@@ -8,4 +8,20 @@ feature 'show average ratings' do
     expect(page).to have_content('4.3')
     expect(page).not_to have_content('4.33')
   end
+
+  scenario 'show average rating for each restaurant on restaurants page' do
+    add_restaurant('Nandos', 'Some chicken in some form')
+    page.all(:link, 'Show')[0].click
+    add_rating(5)
+    add_rating(4)
+    add_rating(4)
+    click_button('Show')
+    add_restaurant('Japanese Kento', 'Lovely sushi')
+    page.all(:link, 'Show')[0].click
+    add_rating(2)
+    add_rating(3)
+    add_rating(5)
+    click_button('Show')
+    
+  end
 end
