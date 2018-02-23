ea1d015e165e3c4057b2778d5b3ca41d131b60fe
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-02-23 10:56:37 +0000

    Add validation in the review model
    co-authored-by: domvernon <3686359+domvernon@users.noreply.github.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :rating, presence: true
-  # numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5},
-  #           message: "%{rating} should be between 1 and 5"
+  validates :rating, presence: true,
+  numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "Review must be between 1 and 5"}
+
 end
