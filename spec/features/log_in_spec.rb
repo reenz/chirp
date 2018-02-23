e109bd8319a96618ab450738cd72a5b909d4e96d
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-02-23 15:10:44 +0000

    Add login test
    co-authored-by: domvernon <3686359+domvernon@users.noreply.github.com>

---
+++ spec/features/log_in_spec.rb
@@ -0,0 +1,10 @@
+feature 'User login' do
+  scenario 'User can login and see their email address on the main restaurant page' do
+    visit 'users/sign_up'
+    fill_in "user[email]", :with => "user@test.com"
+    fill_in 'user[password]' ,:with => "password"
+    fill_in "user[password_confirmation]", :with => "password"
+    click_button "Sign up"
+    expect(page).to have_content("user@test.com page")
+  end
+end
