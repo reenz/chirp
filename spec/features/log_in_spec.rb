8ea871eecc69609fefe282d177d8a85f6126ec7c
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-23 14:31:25 +0000

    failing test for logout

---
+++ spec/features/log_in_spec.rb
@@ -0,0 +1,12 @@
+feature 'User Logout' do
+  scenario 'user can logout' do
+    visit 'users/sign_up'
+    fill_in "user[email]", :with => "user@test.com"
+    fill_in 'user[password]' ,:with => "password"
+    fill_in "user[password_confirmation]", :with => "password"
+    click_button "Sign up"
+    click_link 'Logout'
+    expect(page).to have_current_path('/')
+    expect(page).to have_content('You have logged out: see you soon!')
+  end
+end
