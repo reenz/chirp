80d152a86b37e6f02ecc0df3a9bc67e3d119a14a
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-02-23 11:58:25 +0000

    Remove message from rating validation
    co-authored-by: domvernon <3686359+domvernon@users.noreply.github.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :rating, presence: true,
-  numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "Review must be between 1 and 5"}
-
+  numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
 end
